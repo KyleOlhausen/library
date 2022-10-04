54e4ac801bad69974a7862c486dbc3f1d10bdf88
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-10-04 15:13:32 -0500

    created outline for index.html and main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+let myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+function addBook(book) {
+    myLibrary.push(book);
+}
+
+function deleteBook(book) {
+    
+}
+
+function displayBooks() {
+
+}
