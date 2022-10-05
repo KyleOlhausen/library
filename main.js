0e529da3be8904f2e69f8b005894c554c8e916a1
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-10-04 19:38:48 -0500

    added input form to html and js

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 
 
+const title = document.querySelector('#title-input');
+const author = document.querySelector('#author-input');
+const pages = document.querySelector('#pages-input');
+const status = document.querySelector('#status-input');
+
+
 
 
 let myLibrary = [];
 
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.status = status;
 }
 
 
 function addBook(book) {
-    myLibrary.push(book);
+
+
+
+
+    // myLibrary.push(book);
 }
 
 function deleteBook(book) {
