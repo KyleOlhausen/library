22bf6dcf800ea9c840c92f244a33b64c2de24eb5
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-10-22 17:02:43 -0500

    added styles. Added js to create new book card. Bug clears myLibrary and book cards after execution.

--- main.js
+++ main.js
@@ -3,34 +3,72 @@
 const title = document.querySelector('#title-input');
 const author = document.querySelector('#author-input');
 const pages = document.querySelector('#pages-input');
-const status = document.querySelector('#status-input');
+const read = document.querySelector('#read-input');
+
+const books = document.querySelector('.books');
+
+const submitBtn = document.querySelector('button');
+
+
+submitBtn.addEventListener('click', addBook);
 
 
 
 
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: "A Game of Thrones",
+        author: "George R. R. Martin",
+        pages: 694,
+        read: false
+    }
+];
 
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.read = read;
 }
 
 
-function addBook(book) {
+function addBook() {
+    let newBook = new Book(title.value, author.value, pages.value, read.value);
+    myLibrary.push(newBook);
+    displayBooks();
 
 
-
-
-    // myLibrary.push(book);
 }
+
+
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        const cardTitle = document.createElement('p');
+        const cardAuthor = document.createElement('p');
+        const cardPages = document.createElement('p');
+        const cardRead = document.createElement('button');
+        const cardRemove = document.createElement('button');
+
+        bookCard.classList.add('book-card');
+
+        cardTitle.textContent = book.title;
+        cardAuthor.textContent = book.author;
+        cardPages.textContent = book.pages;
+        cardRead.textContent = book.read;
+
+        bookCard.appendChild(cardTitle);
+        bookCard.appendChild(cardAuthor);
+        bookCard.appendChild(cardPages);
+        bookCard.appendChild(cardRead);
+        bookCard.appendChild(cardRemove);
+        books.appendChild(bookCard);
+    });
+}
+
 
 function deleteBook(book) {
     
 }
 
-function displayBooks() {
-
-}
