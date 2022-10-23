8d29f4f3aaee3e20b396e58b95e1f9ca2287e4fc
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-10-23 17:41:05 -0500

    added function to clear form. Changed read button to show read or notread depending on input

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let myLibrary = [
         title: "A Game of Thrones",
         author: "George R. R. Martin",
         pages: 694,
-        read: false
+        read: "read"
     }
 ];
 
@@ -19,13 +19,23 @@ const $title = document.querySelector('#title-input');
 const $author = document.querySelector('#author-input');
 const $pages = document.querySelector('#pages-input');
 const $read = document.querySelector('#read-input');
-
 const $submitBtn = document.querySelector('#submit-btn');
+const $tbody = document.querySelector('tbody');
+
+
 $submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     addBook();
     displayBooks();
+    clearForm();
 });
+
+function clearForm() {
+    $title.value = "";
+    $author.value = "";
+    $pages.value = "";
+}
+
 
 function addBook(book) {
     if($title.value.length === 0 || $author.value.length === 0 || $pages.value.length === 0){
@@ -36,31 +46,75 @@ function addBook(book) {
     myLibrary.push(newBook);
 }
 
+
+
+
+
+
+
+//need to add a way to select a book
+//using i, add a class of i to each new element
+//to delete remove anything with i class
+//to change read status find i class and change the read btn.textContent
+function displayBooks() {
+    $tbody.textContent = "";
+    myLibrary.forEach((book, i) => {
+
+        let $tr = document.createElement('tr'); 
+        let $tTitle = document.createElement('td')
+        let $tAuthor = document.createElement('td');
+        let $tPages = document.createElement('td');
+        let $readBtn = document.createElement('button');
+        let $tRead = document.createElement('td');
+        let $deleteBtn = document.createElement('button');
+        let $tDelete = document.createElement('td');
+       
+        $tTitle.textContent = book.title;
+        $tAuthor.textContent = book.author;
+        $tPages.textContent = book.pages;
+        $deleteBtn.textContent = "Delete";
+
+        if($read.value){
+            $readBtn.textContent = "Read";
+        }
+        else{
+            $readBtn.textContent = "Not read";
+        }
+
+        $tr.appendChild($tTitle);
+        $tr.appendChild($tAuthor);
+        $tr.appendChild($tPages);
+        $tRead.appendChild($readBtn);
+        $tr.appendChild($tRead);
+        $tDelete.appendChild($deleteBtn);
+        $tr.appendChild($tDelete);
+
+        $tbody.appendChild($tr);
+    });
+}
+
+
+
+function changeReadStatus() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function deleteBook(book) {
     
 }
 
 
-const $tbody = document.querySelector('tbody');
-
-function displayBooks() {
-    $tbody.textContent = "";
-    myLibrary.forEach((book) => {
-        let $tr = document.createElement('tr'); 
-        let $tTitle = document.createElement('td')
-        $tTitle.textContent = book.title;
-        let $tAuthor = document.createElement('td');
-        $tAuthor.textContent = book.author;
-        let $tPages = document.createElement('td');
-        $tPages.textContent = book.pages;
-        let $tRead = document.createElement('td');
-        $tRead.textContent = book.read;
-        $tr.appendChild($tTitle);
-        $tr.appendChild($tAuthor);
-        $tr.appendChild($tPages);
-        $tr.appendChild($tRead);
-        $tbody.appendChild($tr);
-    });
-}
 
 displayBooks();
