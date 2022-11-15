ec35c055ecdf3451e86fa254ceb64d1d8bb2b88c
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-11-15 12:11:55 -0600

    added eventListener to table and deleteBook() and findBook()

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ let myLibrary = [
     }
 ];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read) 
+{
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -19,8 +20,9 @@ const $title = document.querySelector('#title-input');
 const $author = document.querySelector('#author-input');
 const $pages = document.querySelector('#pages-input');
 const $read = document.querySelector('#read-input');
-const $submitBtn = document.querySelector('#submit-btn');
+const $submitBtn = document.querySelector('.submit-btn');
 const $tbody = document.querySelector('tbody');
+
 
 
 $submitBtn.addEventListener('click', (e) => {
@@ -30,15 +32,20 @@ $submitBtn.addEventListener('click', (e) => {
     clearForm();
 });
 
-function clearForm() {
+
+
+function clearForm() 
+{
     $title.value = "";
     $author.value = "";
     $pages.value = "";
 }
 
 
-function addBook(book) {
-    if($title.value.length === 0 || $author.value.length === 0 || $pages.value.length === 0){
+function addBook() 
+{
+    if($title.value.length === 0 || $author.value.length === 0 || $pages.value.length === 0)
+    {
         alert("please fill all fields");
         return;
     }
@@ -56,12 +63,15 @@ function addBook(book) {
 //using i, add a class of i to each new element
 //to delete remove anything with i class
 //to change read status find i class and change the read btn.textContent
-function displayBooks() {
+function displayBooks() 
+{
     $tbody.textContent = "";
-    myLibrary.forEach((book, i) => {
+    myLibrary.forEach((book) => 
+    {
 
         let $tr = document.createElement('tr'); 
         let $tTitle = document.createElement('td')
+        
         let $tAuthor = document.createElement('td');
         let $tPages = document.createElement('td');
         let $readBtn = document.createElement('button');
@@ -69,15 +79,21 @@ function displayBooks() {
         let $deleteBtn = document.createElement('button');
         let $tDelete = document.createElement('td');
        
+        //these are to find the title from btn.dataset.bookTitle
+        $deleteBtn.dataset.bookTitle = book.title;
+        $readBtn.dataset.bookTitle = book.title;
+
         $tTitle.textContent = book.title;
         $tAuthor.textContent = book.author;
         $tPages.textContent = book.pages;
         $deleteBtn.textContent = "Delete";
 
-        if($read.value){
+        if($read.value)
+        {
             $readBtn.textContent = "Read";
         }
-        else{
+        else
+        {
             $readBtn.textContent = "Not read";
         }
 
@@ -95,24 +111,47 @@ function displayBooks() {
 
 
 
-function changeReadStatus() {
 
+const $table = document.querySelector('table').addEventListener('click', e => {
+    if(e.target.textContent == "Delete")
+    {
+        deleteBook(e.target.dataset.bookTitle);
+    }
+    //else if textContent = "Read/not read"
+});
+
+
+
+
+// function changeReadStatus() {
+//     if(.textContent === "Read"){
+//         .textContent = "Not Read";
+//     }
+//     else{
+//         .textContent = "Read";
+//     }
+// }
+
+
+
+function findBook(title) 
+{
+    if(myLibrary.length === 0 || myLibrary === null)
+    {
+        return;
+    }
+
+    return myLibrary.map(e => e.title).indexOf(title);
 }
 
 
+function deleteBook(title) 
+{
+    let bookIndex = findBook(title);
+    if (bookIndex === null || bookIndex === undefined) return;
 
-
-
-
-
-
-
-
-
-
-
-function deleteBook(book) {
-    
+    myLibrary.splice(bookIndex, 1);
+    displayBooks();
 }
 
 
